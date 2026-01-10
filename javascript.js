ab5cac84d30395457ec3743b5ed4edc1a226f4a2
Roxsteam <neves.aleex@gmail.com>
2026-01-09 22:39:58 -0300

    Falta criar função para sortear com repetição e sem

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const QteNumeros = document.querySelector("#numbers")
+const Qtemin = document.querySelector("#start-number")
+const Qtemax = document.querySelector("#end-number")
+const btnSortear = document.querySelector("button")
+const btnNorepeat = document.querySelector(".label-option")
+
